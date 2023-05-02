d3e178ec1839a557a302181db8aad7e5619c073b
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-05-03 01:17:15 +0300

    Create Service for getting comments on post

--- src/services/posts.service.ts
+++ src/services/posts.service.ts
@@ -1,10 +1,15 @@
 import axios from "axios";
 import {BASE_URL} from "@constants/url.ts";
 import {PostType} from "@type/post.type.ts";
+import {CommentType} from "@type/comment.type.ts";
 
 export const postsService = {
-  async getPosts(){
+  async getPosts() {
     const {data} = await axios.get<PostType[]>(`${BASE_URL}/posts?_limit=20`)
+    return data
+  },
+  async getComments(id: string) {
+    const {data} = await axios.get<CommentType[]>(`${BASE_URL}/posts/${id}/comments`)
     return data
   }
 }
--- src/store/posts/posts.slice.ts
+++ src/store/posts/posts.slice.ts
@@ -1,18 +1,22 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {postsService} from "@services/posts.service.ts";
 import {PostType} from "@type/post.type.ts";
+import {CommentType} from "@type/comment.type.ts";
 
 type InitialStateType = {
   loading: boolean,
-  posts: PostType[]
+  posts: PostType[],
+  comments: CommentType[]
 }
 
 const initialState: InitialStateType = {
   loading: false,
-  posts: []
+  posts: [],
+  comments: []
 }
 
 export const fetchPosts = createAsyncThunk('postsSlice/fetchPosts', postsService.getPosts)
+export const getComments = createAsyncThunk('postsSlice/getComments', (id: string) => postsService.getComments(id))
 
 export const postsSlice = createSlice({
   name: 'postsSlice',
@@ -25,10 +29,22 @@ export const postsSlice = createSlice({
         },
     )
     builder.addCase(fetchPosts.fulfilled, (state, action) => {
-      state.posts = action.payload
-      state.loading = false
-    })
+          state.posts = action.payload
+          state.loading = false
+        }
+    )
+    builder.addCase(
+        getComments.pending, state => {
+          state.loading = true
+        }
+    )
+    builder.addCase(
+        getComments.fulfilled, (state, action) => {
+          state.comments = action.payload
+          state.loading = false
+        }
+    )
   }
 })
 
-export  default postsSlice.reducer
+export default postsSlice.reducer
